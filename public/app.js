cad5cd6785f160f1ffffc0587ae7ad83a60c5f84
Jesse Mavi <jesse.mavi@gmail.com>
2016-10-18 17:58:19 -0700

    Adds minor styling

--- public/app.js
+++ public/app.js
@@ -7,7 +7,7 @@ var app = angular.module('taskManagerApp', []);
 app.controller('appController', function($scope, $http) {
 
   $scope.formData = {};
-  $scope.gifs = ['<iframe src="//giphy.com/embed/2vA33ikUb0Qz6" width="480" height="384" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>', '<iframe src="//giphy.com/embed/xT0BKAB7vMb10rfnvG" width="480" height="359" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>'];
+  $scope.gifs = ['<iframe src="//giphy.com/embed/2vA33ikUb0Qz6" width="480" height="384" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>', '<iframe src="//giphy.com/embed/xT0BKAB7vMb10rfnvG" width="480" height="359" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>', '<iframe src="//giphy.com/embed/3o7qDEq2bMbcbPRQ2c" width="480" height="333" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>', '<iframe src="//giphy.com/embed/Mp4hQy51LjY6A" width="480" height="320" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>', '<iframe src="//giphy.com/embed/eoxomXXVL2S0E" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>', '<iframe src="//giphy.com/embed/10ERZqYioLWJ6U" width="480" height="330" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>', '<iframe src="//giphy.com/embed/KEVNWkmWm6dm8" width="480" height="273" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>', '<iframe src="//giphy.com/embed/MteOtn95GNx5e" width="480" height="398" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>'];
 
   // on page load make the following requests:
 
@@ -38,7 +38,7 @@ app.controller('appController', function($scope, $http) {
         $scope.tasks = taskData;
         $scope.formData = {};
         console.log(taskData.length);
-        // angular.element('.gifDiv').empty();
+        angular.element('.gif').empty();
       })
       .error(function(taskData) {
         console.log('error posting task', taskData);
@@ -54,8 +54,8 @@ app.controller('appController', function($scope, $http) {
         $scope.tasks = taskData;
         console.log(taskData.length);
         angular.element('.gif').empty();
-        var svgTag = angular.element('<iframe src="//giphy.com/embed/2vA33ikUb0Qz6" width="480" height="384" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>');
-        angular.element(svgTag).appendTo('.gif');
+        var gif = $scope.gifs[Math.floor(Math.random() * $scope.gifs.length)];
+        angular.element(gif).appendTo('.gif');
       })
       .error(function(taskData) {
         console.log('error deleting task', taskData);
